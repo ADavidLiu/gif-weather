61b40053e70c4ac4e2e01bd4e4182af0e4f8602d
Andrés David Liu Fong <adavidliuf@gmail.com>
2018-09-14 13:16:26 -0500

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'weather-gifs',
     environment,
-    rootURL: '/dist/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
